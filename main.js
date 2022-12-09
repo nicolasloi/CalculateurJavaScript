45f7cd000363997a396bb922dd00c61ece97965c
nicolasloison13 <nicolasloison13@icloud.com>
2022-12-09 14:48:35 +0100

    add h4 under the table

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
-
-
-function addRow() {
-  var table = document.getElementById("+");
-  var row = table.insertRow(0);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  cell1.innerHTML = "NEW CELL1";
-  cell2.innerHTML = "NEW CELL2";
-}
-
+// test delet last row in table (only for "ecole-pro")
 function deleteRow() {
   document.getElementById("table_ecole-pro").deleteRow(-1);
 }
 
-document.getElementById("add_ecole-pro").addEventListener("click",function() {
-  addRow()
+document.getElementById("del_ecole-pro").addEventListener("click", function () {
+  deleteRow();
 });
-
-document.getElementById("del_ecole-pro").addEventListener("click",function() {
-  deleteRow()
-});
-
-  
