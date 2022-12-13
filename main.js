10ce3ab194040f4742b04a1e1f1b9330ada7e0fe
nicolasloison13 <nicolasloison13@icloud.com>
2022-12-13 13:37:15 +0100

    add button on mid table + css + test JS

--- main.js
+++ main.js
@@ -1,8 +1,67 @@
+// creation des objets
+
+let notes = {
+
+  ecole_pro: {
+      semestre1: [],
+      mod1: [],
+      moy: [],
+  },
+
+  cours_inter: {
+      semestre1: [],
+      mod1: [],
+      moy: [],
+  },
+
+  cdb: {
+      mathSemestre1: [],
+      mathSemestre2: [],
+      mathSemestre3: [],
+      anglaisSemestre1: [],
+      anglaisSemestre2: [],
+      anglaisSemestre3: [],
+      anglaisSemestre4: [],
+      anglaisSemestre5: [],
+      moy: [],
+  },
+
+  culture_G: {
+      cultGSemestre1: [],
+      cultGSemestre2: [],
+      cultGSemestre3: [],
+      cultGSemestre4: [],
+      cultGSemestre5: [],
+      cultGSemestre6: [],
+      cultGSemestre7: [],
+      cultGSemestre8: [],
+      moy: [],
+  },
+
+  TPI: {
+      semestre1: [],
+      mod1: [],
+      moy: [],
+  },
+};
+
+
+let table = document.getElementsByClassName("table")
+console.log(table)
+
+
+// function for add new row in table
+function addButton(index) {
+  document.getElementById("addButton-" + index).addEventListener("click", function (){
+
+  });
+}
+
 // test delet last row in table (only for "ecole-pro")
 function deleteRow() {
   document.getElementById("table_ecole-pro").deleteRow(-1);
 }
 
-document.getElementById("del_ecole-pro").addEventListener("click", function () {
+document.getElementById("delButton-1").addEventListener("click", function () {
   deleteRow();
 });
